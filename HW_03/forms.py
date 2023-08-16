483a01b3e3f54c5660c315456694c04b8d834c1b
Anna <annkrv80@yandex.ru>
2023-08-16 23:43:23 +0700

    HW_03

---
+++ HW_03/forms.py
@@ -0,0 +1,16 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+from wtforms.validators import DataRequired,EqualTo
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    surname = StringField('Surname', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
---
+++ HW_03/main.py
@@ -0,0 +1,60 @@
+#Создать форму для регистрации пользователей на сайте. 
+#Форма должна содержать поля "Имя", "Фамилия", "Email", "Пароль" и кнопку "Зарегистрироваться". 
+#При отправке формы данные должны сохраняться в базе данных, а пароль должен быть зашифрован.
+
+from flask import Flask, render_template, request
+from flask_wtf import FlaskForm
+from flask_wtf.csrf import CSRFProtect
+from models import db, User
+from forms import RegistrationForm
+from werkzeug.security import generate_password_hash, check_password_hash
+import re
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mysecretkey'
+csrf = CSRFProtect(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///users.db'
+db.init_app(app)
+
+
+@app.cli.command("init-db")
+def init_db():
+    db.create_all()
+
+
+@app.route('/')
+def index():
+    return 'Наш сайт'
+
+
+pattern = r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$'
+
+
+@app.route('/register/', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if request.method == 'POST' and form.validate():
+        username =form.username.data
+        surname = form.surname.data
+        email = form.email.data
+        psw = form.password.data
+        if re.match(pattern, psw):
+            password = generate_password_hash(psw)
+        else:
+            return f'Пароль должен содержать буквы в верхнем и нижнем регистре, хотя бы одну цифру и 8 символов'
+        existing_user = User.query.filter(User.email == email).first()
+        if existing_user:
+            error_msg = 'Пользователь с таким email уже существует.'
+            form.username.errors.append(error_msg)
+            return render_template('register.html', form=form)
+        new_user = User(username=username, surname=surname, email=email, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+
+       
+        success_msg = 'Регистрация прошла успешно!'
+        return success_msg
+
+    return render_template('register.html', form=form)
+
+
